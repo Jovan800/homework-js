bbc7a1ce24c65e794ca13df7da2700c7f2c88d8d
Jovan800 <jovanatanasov800@gmail.com>
2023-12-05 15:13:55 +0100

    homework-js1

---
+++ domasno-js1/script.js
@@ -0,0 +1,7 @@
+let price = 119.95;
+let tax = ((5/100)*price)
+console.log ('Tax for one phone is: ' , tax)
+let phones = 30*price
+console.log ( 'Price for 30 phones is: ', phones )
+let priceTax = phones+(tax*30 )
+console.log ('Price for 30 phones with tax is :',priceTax )
